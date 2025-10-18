afb1bee57af28d6a70a5c4dd85d9ab9036bb8ef0
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 01:26:46 +0530

    Refactor PatientRepository to extend JpaRepository

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/PatientRepository.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/PatientRepository.java
@@ -1,5 +1,14 @@
 package com.miliproject.springboot.repository;
 
-public class PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.miliproject.springboot.model.Patient;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+    Optional<Patient> findByPhone(String phone);
 }
