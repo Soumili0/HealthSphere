dbf43e36e9f0df0b7f09d14a5c73cf316a3f4f78
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 01:26:25 +0530

    Update EHRRepository.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/EHRRepository.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/EHRRepository.java
@@ -1,5 +1,17 @@
 package com.miliproject.springboot.repository;
 
-public class EHRRepository {
+import com.miliproject.springboot.model.EHRRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface EHRRepository extends JpaRepository<EHRRecord, Long> {
+
+    // নির্দিষ্ট রোগীর সব EHR রেকর্ড
+    List<EHRRecord> findByPatientId(Long patientId);
+
+    // নির্দিষ্ট ডাক্তারের সব রেকর্ড
+    List<EHRRecord> findByDoctorId(Long doctorId);
 }
