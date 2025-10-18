1114322eea5e0f6300592b8134c291795b20f879
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:15:35 +0530

    Update VideocallService.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/service/VideocallService.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/service/VideocallService.java
@@ -3,10 +3,12 @@ package com.miliproject.springboot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.miliproject.springboot.model.Patient;
 import com.miliproject.springboot.model.Videocall;
 import com.miliproject.springboot.repository.VideocallRepository;
 import com.miliproject.springboot.repository.VideocallRepository.VideoCallRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,19 +22,19 @@ public class VideocallService {
         this.videoCallRepository = videoCallRepository;
     }
 
-    public List<VideocallRepository> getAllCalls() {
-        return videoCallRepository.findAll();
+    public List<Videocall> getAllCalls() {
+        return (List<Videocall>) videoCallRepository.findAll();
     }
 
-    public Optional<VideocallRepository> getCallById(Long id) {
-        return videoCallRepository.findById(id);
+    public Optional<Videocall> getCallById(Long id) {
+        return (Optional<Videocall>) videoCallRepository.findById(id);
     }
 
     public Videocall scheduleCall(Videocall call) {
         return videoCallRepository.save(call);
     }
 
-    public VideocallRepository updateCall(Long id, Videocall updatedCall) {
+    public Videocall updateCall(Long id, VideocallService updatedCall) {
         return videoCallRepository.findById(id).map(call -> {
             call.setDoctor(updatedCall.getDoctor());
             call.setPatient(updatedCall.getPatient());
@@ -43,7 +45,32 @@ public class VideocallService {
         }).orElseThrow(() -> new RuntimeException("Video call not found with id: " + id));
     }
 
-    public void cancelCall(Long id) {
+    private String getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private LocalDateTime getEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private LocalDateTime getStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Patient getPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Object getDoctor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void cancelCall(Long id) {
         if (!videoCallRepository.existsById(id)) {
             throw new RuntimeException("Video call not found with id: " + id);
         }
