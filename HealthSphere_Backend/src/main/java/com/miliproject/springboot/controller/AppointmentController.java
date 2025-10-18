c2df234f18ed5203d9528c81aac231001d023654
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:22:38 +0530

    Update AppointmentController.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/AppointmentController.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/AppointmentController.java
@@ -1,5 +1,37 @@
 package com.miliproject.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.miliproject.springboot.model.Appointment;
+import com.miliproject.springboot.service.AppointmentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/appointments")
 public class AppointmentController {
 
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @PostMapping
+    public Appointment createAppointment(@RequestBody Appointment appointment) {
+        return appointmentService.saveAppointment(appointment);
+    }
+
+    @GetMapping
+    public List<Appointment> getAllAppointments() {
+        return appointmentService.getAllAppointments();
+    }
+
+    @GetMapping("/patient/{patientId}")
+    public List<Appointment> getAppointmentsByPatient(@PathVariable Long patientId) {
+        return appointmentService.getAppointmentsByPatient(patientId);
+    }
+
+    @GetMapping("/doctor/{doctorId}")
+    public List<Appointment> getAppointmentsByDoctor(@PathVariable Long doctorId) {
+        return appointmentService.getAppointmentsByDoctor(doctorId);
+    }
 }
