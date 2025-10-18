47a05da4d6b789d86dc53cd622cc7da29a56713e
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 01:12:27 +0530

    Update HealthSphereApplication.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/HealthSphereApplication.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/HealthSphereApplication.java
@@ -3,11 +3,11 @@ package com.miliproject.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.miliproject.springboot")
 public class HealthSphereApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HealthSphereApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HealthSphereApplication.class, args);
+    }
 
 }
