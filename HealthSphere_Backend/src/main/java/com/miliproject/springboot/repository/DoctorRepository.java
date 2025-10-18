2bf2e60c9a99f91432558fbaf126426fa6e0e9ac
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 01:26:03 +0530

    Refactor DoctorRepository to use JpaRepository

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/DoctorRepository.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/repository/DoctorRepository.java
@@ -1,5 +1,14 @@
 package com.miliproject.springboot.repository;
 
-public class DoctorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.miliproject.springboot.model.Doctor;
+
+import java.util.Optional;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Optional<Doctor> findByEmail(String email);
+    Optional<Doctor> findByPhone(String phone);
 }
