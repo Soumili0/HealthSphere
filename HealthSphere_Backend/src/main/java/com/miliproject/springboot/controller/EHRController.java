50cdaf574ddf967ef5f435a2766d17c92ccb7949
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:23:52 +0530

    Update EHRController.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/EHRController.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/EHRController.java
@@ -1,5 +1,26 @@
 package com.miliproject.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.miliproject.springboot.dto.EHRDTO;
+import com.miliproject.springboot.service.EHRService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ehr")
 public class EHRController {
 
+    @Autowired
+    private EHRService ehrService;
+
+    /**
+     * Fetch all EHR records for a patient
+     * Example: GET /api/ehr/patient/1
+     */
+    @GetMapping("/patient/{patientId}")
+    public List<EHRDTO> getPatientEHR(@PathVariable Long patientId) {
+        return ehrService.getPatientEHR(patientId);
+    }
 }
