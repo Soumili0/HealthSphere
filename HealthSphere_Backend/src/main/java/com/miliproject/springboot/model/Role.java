ea43a83efae70f6fbe7089bc99f851d684db1c20
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 01:20:01 +0530

    Add Lombok annotations to Role class

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Role.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Role.java
@@ -1,17 +1,21 @@
 package com.miliproject.springboot.model;
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "roles")
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
-   @Id
+
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique=true, nullable=false)
-    private String name; // ROLE_PATIENT, ROLE_DOCTOR, ROLE_ADMIN
-
-    // getters/setters
-
+    @Column(nullable = false, unique = true)
+    private String name; // উদাহরণ: "ADMIN", "DOCTOR", "PATIENT", "USER"
 }
