251513923dd229c39b4853863b3c04df9408792f
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:18:22 +0530

    Update AppointmentDTO.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/AppointmentDTO.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/AppointmentDTO.java
@@ -1,6 +1,18 @@
 package com.miliproject.springboot.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppointmentDTO {
-
+    private Long id;
+    private Long patientId;
+    private Long doctorId;
+    private LocalDateTime appointmentDate;
+    private String status; // scheduled, completed, canceled
 }
-
