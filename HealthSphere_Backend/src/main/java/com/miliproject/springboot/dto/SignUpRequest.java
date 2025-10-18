4d6c443a2feda40a84f7e094265c8ed1a8c4f67f
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:21:48 +0530

    Update SignUpRequest.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/SignUpRequest.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/SignUpRequest.java
@@ -1,6 +1,42 @@
-
 package com.miliproject.springboot.dto;
 
-public class SignUpRequest {
+import lombok.Data;
 
+@Data
+public class SignUpRequestsDTO {
+    private String fullName;
+    private String email;   // optional
+    private String phone;   // optional
+    private String password;
+
+    // Utility method to check how many OTPs to send
+    public int otpCount() {
+        int count = 0;
+        if(email != null && !email.isEmpty()) count++;
+        if(phone != null && !phone.isEmpty()) count++;
+        return count;
+    }
+
+	
+
+	public Object getPhone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	public Object getFullName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	public Object getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
+
+
