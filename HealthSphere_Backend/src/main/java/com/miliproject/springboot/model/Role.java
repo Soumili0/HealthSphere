646e2d549ced258e7d9d3fb26ea78a4a462c4a45
Anant Soam <asoam6011@gmail.com>
2025-10-18 10:47:52 +0530

    Create Role entity with id and name attributes
    Added Role entity with id and name fields.

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Role.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Role.java
@@ -1,5 +1,17 @@
 package com.miliproject.springboot.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 
 public class Role {
+   @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable=false)
+    private String name; // ROLE_PATIENT, ROLE_DOCTOR, ROLE_ADMIN
+
+    // getters/setters
 
 }
