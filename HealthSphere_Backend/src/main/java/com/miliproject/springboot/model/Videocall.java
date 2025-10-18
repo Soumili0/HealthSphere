67a5ea05980b252054bf681b86c650e3475c61f2
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:17:22 +0530

    Update Videocall.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Videocall.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Videocall.java
@@ -26,4 +26,54 @@ public class Videocall {
     private String callLink; // Zoom / Jitsi / custom link
 
     private String status; // Scheduled, Completed, Cancelled
+
+	public Object getDoctor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getPatient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getStartTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getEndTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setDoctor(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPatient(Patient patient) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setStartTime(LocalDateTime startTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEndTime(LocalDateTime endTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setStatus(String status2) {
+		// TODO Auto-generated method stub
+		
+	}
 }
