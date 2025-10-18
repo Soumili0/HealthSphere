4f3f2458a7121e97b3782ecd6a893699814c091c
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:23:28 +0530

    Update DoctorController.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/DoctorController.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/DoctorController.java
@@ -1,5 +1,32 @@
 package com.miliproject.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.miliproject.springboot.model.Doctor;
+import com.miliproject.springboot.service.DoctorService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping
+    public Doctor createDoctor(@RequestBody Doctor doctor) {
+        return doctorService.saveDoctor(doctor);
+    }
+
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/{id}")
+    public Doctor getDoctorById(@PathVariable Long id) {
+        return doctorService.getDoctorById(id);
+    }
 }
