9b736ffeb83fd2525d2e1a9a45ab2fad7f8aea63
Anant Soam <asoam6011@gmail.com>
2025-10-18 11:18:11 +0530

    Update Doctor.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Doctor.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/model/Doctor.java
@@ -1,6 +1,21 @@
-
 package com.miliproject.springboot.model;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "doctors")
 
 public class Doctor {
+   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String specialization;
+    private String email;
+    private String phone;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
