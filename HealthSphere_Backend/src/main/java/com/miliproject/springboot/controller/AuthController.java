ac91bf53d7cbedcfdbe7c6b2db18d87d23e7d19f
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:23:05 +0530

    Update AuthController.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/AuthController.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/AuthController.java
@@ -1,5 +1,20 @@
 package com.miliproject.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.miliproject.springboot.dto.SignUpRequestsDTO;
+import com.miliproject.springboot.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public String signUp(@RequestBody SignUpRequestsDTO signUpRequest) {
+        return authService.signUp(signUpRequest);
+    }
 }
