166927f58ceadea927e8a298e48dec4c03db688d
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:21:21 +0530

    Update PrescriptionDTO.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/PrescriptionDTO.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/PrescriptionDTO.java
@@ -1,6 +1,14 @@
-
 package com.miliproject.springboot.dto;
 
-public class PrescriptionDTO {
+import lombok.Data;
+import java.time.LocalDate;
 
+@Data
+public class PrescriptionDTO {
+    private Long id;
+    private Long patientId;
+    private Long doctorId;
+    private String diagnosis;
+    private String medications; // e.g., "Drug A 5mg, Drug B 10mg"
+    private LocalDate date;
 }
