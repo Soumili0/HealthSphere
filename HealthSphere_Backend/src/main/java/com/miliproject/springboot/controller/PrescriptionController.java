c796fa086d3769abe94088ff1db2d10dfb997a72
Soumili Samanta <soumili.samanta18@gmail.com>
2025-10-19 02:25:54 +0530

    Update PrescriptionController.java

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/PrescriptionController.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/controller/PrescriptionController.java
@@ -1,5 +1,65 @@
 package com.miliproject.springboot.controller;
 
+import com.miliproject.springboot.model.Prescription;
+import com.miliproject.springboot.service.PresciptionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/prescriptions")
 public class PrescriptionController {
 
+    private final PresciptionService prescriptionService;
+
+    @Autowired
+    public PrescriptionController(PresciptionService prescriptionService) {
+        this.prescriptionService = prescriptionService;
+    }
+
+    // Get all prescriptions
+    @GetMapping
+    public List<Prescription> getAllPrescriptions() {
+        return prescriptionService.getAllPrescriptions();
+    }
+
+    // Get a prescription by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Prescription> getPrescriptionById(@PathVariable Long id) {
+        return prescriptionService.getPrescriptionById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Add a new prescription
+    @PostMapping
+    public Prescription addPrescription(@RequestBody Prescription prescription) {
+        return prescriptionService.addPrescription(prescription);
+    }
+
+    // Update an existing prescription
+    @PutMapping("/{id}")
+    public ResponseEntity<Prescription> updatePrescription(
+            @PathVariable Long id, @RequestBody Prescription updatedPrescription) {
+        try {
+            Prescription prescription = prescriptionService.updatePrescription(id, updatedPrescription);
+            return ResponseEntity.ok(prescription);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete a prescription
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePrescription(@PathVariable Long id) {
+        try {
+            prescriptionService.deletePrescription(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
