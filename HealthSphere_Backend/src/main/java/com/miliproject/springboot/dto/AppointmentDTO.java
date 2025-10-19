cb618ca75eda7bebbef452c50a6fb90b1826789e
Gayatri Sutar <gayatrisutar10170@gmail.com>
2025-10-19 12:48:00 +0530

    added final dto

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/AppointmentDTO.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/AppointmentDTO.java
@@ -6,13 +6,38 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * Data Transfer Object for Appointment entity.
+ * Used to transfer only necessary appointment data between layers or APIs.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppointmentDTO {
+
+    /**
+     * Appointment ID
+     */
     private Long id;
+
+    /**
+     * ID of the patient for this appointment
+     */
     private Long patientId;
+
+    /**
+     * ID of the doctor for this appointment
+     */
     private Long doctorId;
+
+    /**
+     * Scheduled date and time for the appointment
+     */
     private LocalDateTime appointmentDate;
-    private String status; // scheduled, completed, canceled
+
+    /**
+     * Status of the appointment
+     * Possible values: "SCHEDULED", "COMPLETED", "CANCELLED"
+     */
+    private String status;
 }
