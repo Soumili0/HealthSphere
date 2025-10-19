6d7ad8dae142b1ce08ae915198fbc43be246aecb
Gayatri Sutar <gayatrisutar10170@gmail.com>
2025-10-19 12:48:35 +0530

    added final dto

--- HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/DoctorDTO.java
+++ HealthSphere_Backend/src/main/java/com/miliproject/springboot/dto/DoctorDTO.java
@@ -4,17 +4,52 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Data Transfer Object for Doctor entity.
+ * Used to transfer only necessary doctor data between layers or APIs.
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class DoctorDTO {
 
+    /**
+     * Doctor ID
+     */
     private Long id;
+
+    /**
+     * Full name of the doctor
+     */
     private String fullName;
+
+    /**
+     * Specialization of the doctor (e.g., Cardiologist, Dermatologist)
+     */
     private String specialization;
+
+    /**
+     * Email of the doctor
+     */
     private String email;
+
+    /**
+     * Phone number of the doctor
+     */
     private String phone;
+
+    /**
+     * Address of the doctor
+     */
     private String address;
+
+    /**
+     * Qualification of the doctor (e.g., MBBS, MD)
+     */
     private String qualification;
+
+    /**
+     * Years of experience
+     */
     private int experience;
 }
